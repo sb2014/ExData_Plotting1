d6342b7b4c58229615d4b333240c0c0f58a061e5
sb2014 <suvrat.bansal@gmail.com>
2014-05-10 08:38:25 -0400

    Committing Four R files and corresponding plot.png

--- plot3.R
+++ plot3.R
@@ -5,7 +5,7 @@ subDat <- subset(dat, dat$Date == "2007-02-01" | dat$Date =="2007-02-02")
 plot(as.POSIXct(paste(subDat$Date, subDat$Time), format="%Y-%m-%d %H:%M:%S"), as.numeric(as.character(subDat$Sub_metering_1)), ylab="Energy sub metering", xlab="", type="l")
 lines(as.POSIXct(paste(subDat$Date, subDat$Time), format="%Y-%m-%d %H:%M:%S"), as.numeric(as.character(subDat$Sub_metering_2)), type="l", col="red")
 lines(as.POSIXct(paste(subDat$Date, subDat$Time), format="%Y-%m-%d %H:%M:%S"), as.numeric(as.character(subDat$Sub_metering_3)), type="l", col="blue")
-legend("topright", lty=1, bty="n", col=c("black", "blue", "red"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), text.font=1,cex=0.6)
+legend("topright", lty=1,col=c("black", "blue", "red"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), text.font=1,cex=0.6)
 
 dev.copy(png, file = "plot3.png", width = 480, height = 480, units = "px", res=60) ## Copy my plot to a PNG file 
 par(mar=c(4,3,2,2)+0.1)
